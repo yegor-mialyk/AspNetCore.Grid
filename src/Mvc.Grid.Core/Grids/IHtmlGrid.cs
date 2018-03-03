b54220acd0559d6626bb7e82f7e108668f93a4d6
Muchiachio <aldis.rameika@gmail.com>
2018-03-03 17:07:14 +0200

    Html grid extensions
    - Moved html grid helper methods to extensions, to improve extensibility
    by lowering API surface which needs to be implemented.

--- src/Mvc.Grid.Core/Grids/IHtmlGrid.cs
+++ src/Mvc.Grid.Core/Grids/IHtmlGrid.cs
@@ -7,25 +7,5 @@ namespace NonFactors.Mvc.Grid
     {
         IGrid<T> Grid { get; }
         String PartialViewName { get; set; }
-
-        IHtmlGrid<T> Build(Action<IGridColumnsOf<T>> builder);
-        IHtmlGrid<T> ProcessWith(IGridProcessor<T> processor);
-        IHtmlGrid<T> WithSourceUrl(String url);
-        
-        IHtmlGrid<T> MultiFilterable();
-        IHtmlGrid<T> Filterable();
-        IHtmlGrid<T> Sortable();
-
-        IHtmlGrid<T> RowAttributed(Func<T, Object> htmlAttributes);
-        IHtmlGrid<T> RowCss(Func<T, String> cssClasses);
-        IHtmlGrid<T> Attributed(Object htmlAttributes);
-        IHtmlGrid<T> Css(String cssClasses);
-        IHtmlGrid<T> Empty(String text);
-        IHtmlGrid<T> Named(String name);
-
-        IHtmlGrid<T> WithFooter(String partialViewName);
-
-        IHtmlGrid<T> Pageable(Action<IGridPager<T>> builder);
-        IHtmlGrid<T> Pageable();
     }
 }
---
+++ src/Mvc.Grid.Core/Html/HtmlGridExtensions.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace NonFactors.Mvc.Grid
+{
+    public static class HtmlGridExtensions
+    {
+        public static IHtmlGrid<T> Build<T>(this IHtmlGrid<T> html, Action<IGridColumnsOf<T>> builder)
+        {
+            builder(html.Grid.Columns);
+
+            return html;
+        }
+        public static IHtmlGrid<T> ProcessWith<T>(this IHtmlGrid<T> html, IGridProcessor<T> processor)
+        {
+            html.Grid.Processors.Add(processor);
+
+            return html;
+        }
+        public static IHtmlGrid<T> WithSourceUrl<T>(this IHtmlGrid<T> html, String url)
+        {
+            html.Grid.SourceUrl = url;
+
+            return html;
+        }
+
+        public static IHtmlGrid<T> MultiFilterable<T>(this IHtmlGrid<T> html)
+        {
+            foreach (IGridColumn<T> column in html.Grid.Columns)
+            {
+                if (column.IsFilterable == null)
+                    column.IsFilterable = true;
+
+                if (column.IsMultiFilterable == null)
+                    column.IsMultiFilterable = true;
+            }
+
+            return html;
+        }
+        public static IHtmlGrid<T> Filterable<T>(this IHtmlGrid<T> html)
+        {
+            foreach (IGridColumn<T> column in html.Grid.Columns)
+                if (column.IsFilterable == null)
+                    column.IsFilterable = true;
+
+            return html;
+        }
+        public static IHtmlGrid<T> Sortable<T>(this IHtmlGrid<T> html)
+        {
+            foreach (IGridColumn<T> column in html.Grid.Columns)
+                if (column.IsSortable == null)
+                    column.IsSortable = true;
+
+            return html;
+        }
+
+        public static IHtmlGrid<T> RowAttributed<T>(this IHtmlGrid<T> html, Func<T, Object> htmlAttributes)
+        {
+            html.Grid.Rows.Attributes = htmlAttributes;
+
+            return html;
+        }
+        public static IHtmlGrid<T> RowCss<T>(this IHtmlGrid<T> html, Func<T, String> cssClasses)
+        {
+            html.Grid.Rows.CssClasses = cssClasses;
+
+            return html;
+        }
+        public static IHtmlGrid<T> Attributed<T>(this IHtmlGrid<T> html, Object htmlAttributes)
+        {
+            html.Grid.Attributes = new GridHtmlAttributes(htmlAttributes);
+
+            return html;
+        }
+        public static IHtmlGrid<T> Css<T>(this IHtmlGrid<T> html, String cssClasses)
+        {
+            html.Grid.CssClasses = cssClasses;
+
+            return html;
+        }
+        public static IHtmlGrid<T> Empty<T>(this IHtmlGrid<T> html, String text)
+        {
+            html.Grid.EmptyText = text;
+
+            return html;
+        }
+        public static IHtmlGrid<T> Named<T>(this IHtmlGrid<T> html, String name)
+        {
+            html.Grid.Name = name;
+
+            return html;
+        }
+
+        public static IHtmlGrid<T> WithFooter<T>(this IHtmlGrid<T> html, String partialViewName)
+        {
+            html.Grid.FooterPartialViewName = partialViewName;
+
+            return html;
+        }
+
+        public static IHtmlGrid<T> Pageable<T>(this IHtmlGrid<T> html, Action<IGridPager<T>> builder)
+        {
+            html.Grid.Pager = html.Grid.Pager ?? new GridPager<T>(html.Grid);
+            builder(html.Grid.Pager);
+
+            if (!html.Grid.Processors.Contains(html.Grid.Pager))
+                html.Grid.Processors.Add(html.Grid.Pager);
+
+            return html;
+        }
+        public static IHtmlGrid<T> Pageable<T>(this IHtmlGrid<T> html)
+        {
+            return html.Pageable(builder => { });
+        }
+    }
+}
